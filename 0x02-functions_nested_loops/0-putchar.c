617afdfce201d4ce7a310632f0ef642136a444c6
mlokole <100201539+mlokole@users.noreply.github.com>
2022-07-05 13:16:58 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-#include <main.h>
+#include <alx.h>
 
 /**
  * main - Check description
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char word[9] = "_putchar";
+	char word[9] = "alx";
 	int i;
 
 	for (i = 0; i < 9; i++)
