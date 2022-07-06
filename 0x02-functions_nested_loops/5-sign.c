95e02e0cb32a9a30d2617aa624c367424270a608
mlokole <sam.njir@gmail.com>
2022-07-06 08:03:38 -0700

    5-main.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,11 @@
+#include "main.h"
+
+/**
+ * print sign - function
+ *
+ * @n: character to compare
+ *
+ * Return: 1 if n is greater than zero, 0 if n is equal to zero and -1 if n is less than zero
+ *
+ */
 
