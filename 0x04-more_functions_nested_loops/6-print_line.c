1690cc4168a3e1665bc64cbe14b9dccefaab07bd
mlokole <100201539+mlokole@users.noreply.github.com>
2022-07-07 17:16:36 +0300

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - function that prints a line
+ *
+ * @n: number of times the character _ should be printed
+ *
+ * Return: a new line if n is 0 or less, otherwise a straight line
+ */
+
+void print_line(int n)
+{
+	int l;
+
+	for (l = 0; l < n; l++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
