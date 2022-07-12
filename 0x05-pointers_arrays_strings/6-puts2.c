91a57f5f444b83b646d7b888f7bd3adfaf3acd72
mlokole <sam.njir@gmail.com>
2022-07-12 03:47:24 -0700

    6-puts.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "2-strlen.c"
 
 /**
  * puts2 - prints xters of a string
@@ -8,15 +9,14 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int a;
 
-	while (str[i] != '\0')
+	for (a = 0; a < _strlen(str); a++)
 	{
-		if (i % 2 == 0 )
+		if (a % 2 == 0)
 		{
-			_putchar(str[i]);
+			_putchar(str[a]);
 		}
-		i++;
 	}
 	_putchar('\n');
 }
